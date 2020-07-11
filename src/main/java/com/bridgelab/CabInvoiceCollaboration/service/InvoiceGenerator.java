b45f3416823b37b3f3fcca115464fe986715eca5
“sandip5” <singhsandeep.singh97@gmail.com>
2020-07-11 10:04:25 +0530

    ADDED: Calculate Fare Method For Minimum Fare

--- src/main/java/com/bridgelab/CabInvoiceCollaboration/service/InvoiceGenerator.java
+++ src/main/java/com/bridgelab/CabInvoiceCollaboration/service/InvoiceGenerator.java
@@ -3,11 +3,11 @@ package com.bridgelab.CabInvoiceCollaboration.service;
 public class InvoiceGenerator {
     private static final double MINIMUM_COST_PER_KM =10 ;
     private static final int COST_PER_TIME = 1;
-    private static final double MINIMUM_FARE = 5;
+    private static final double MINIMUM_FARE_FOR_RIDES = 5;
 
     public double calculateFare(double distance, int time)
     {
         double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
-        return Math.max(totalFare, MINIMUM_FARE);
+        return Math.max(totalFare, MINIMUM_FARE_FOR_RIDES);
     }
 }
